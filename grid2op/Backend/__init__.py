720324fb5a89aa5a6b7f23ffb3f4f7e5874f3374
jigarparekh279 <jigarparekh279@gmail.com>
2025-07-29 11:53:22 +0200

    cleaned

--- grid2op/Backend/__init__.py
+++ grid2op/Backend/__init__.py
@@ -1,5 +1,4 @@
-__all__ = ["Backend", "PandaPowerBackend", "OpenDSSBackend"]
+__all__ = ["Backend", "PandaPowerBackend"]
 
 from grid2op.Backend.backend import Backend
 from grid2op.Backend.pandaPowerBackend import PandaPowerBackend
-from grid2op.Backend.openDSSBackend import OpenDSSBackend
